b3f1ffcbefcd89b45373ef1101d39a25de2156ec
Jonathan Lewis <jonathandavidlewis92@gmail.com>
2023-03-25 12:00:40 -0600

    fix: clap version

--- backend/src/config.rs
+++ backend/src/config.rs
@@ -15,13 +15,13 @@
 #[derive(clap::Parser)]
 pub struct Config {
     /// The connection URL for the Postgres database this application should use.
-    #[clap(long, env)]
+    #[arg(long, env)]
     pub database_url: String,
 
     /// The HMAC signing and verification key used for login tokens (JWTs).
     ///
     /// There is no required structure or format to this key as it's just fed into a hash function.
     /// In practice, it should be a long, random string that would be infeasible to brute-force.
-    #[clap(long, env)]
+    #[arg(long, env)]
     pub hmac_key: String,
 }
